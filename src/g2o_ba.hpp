bbbe100d288ff34e361f5e99ef96eefc79f50590
cuihangmver <cuihangmver@outlook.com>
2022-02-25 20:16:05 +0800

    add g2o

--- src/g2o_ba.hpp
+++ src/g2o_ba.hpp
@@ -15,7 +15,23 @@ class OptimizeBA
     {
 
     }
-    void OptimizePoseMulPointsOneFrame(std::vector<Eigen::Vector3d> &vecLandmark, std::vector<Eigen::Vector3d> &vecUv, std::vector<Sophus::SE3d> &vecPose, cv::Mat K)
+
+    int consitute(int i, int j)
+    {
+        int num = 10;
+        while(1)
+        {
+            if(j / num == 0)
+            {
+                break;
+            }
+            num = num * 10;
+        }
+        int cons = i * num + j;
+        return cons;
+    }
+
+    void OptimizePoseMulPointsOneFrame(std::vector<Eigen::Vector3d> &vecLandmark, std::map<int, Eigen::Vector2d> &mapUv, std::vector<Sophus::SE3d> &vecPose, cv::Mat K)
     {
         typedef g2o::BlockSolver_6_3 BlockSolverType;
         typedef g2o::LinearSolverCSparse<BlockSolverType::PoseMatrixType>
@@ -44,8 +60,24 @@ class OptimizeBA
                 optimizer.addVertex(vertex_point);
         }
         // 加优化边
-
-
+        for(int i = 0; i < vecLandmark.size(); i++)
+        {
+            for(int j = 0; j < vecPose.size(); j++)
+            {
+                PosePointEdge *posepointEdge = new PosePointEdge(K);
+                posepointEdge->setId(consitute(i, j));
+                posepointEdge->setVertex(0, vecPose[j]);         // pose
+                posepointEdge->setVertex(1, vecLandmark[i]);     // point
+                posepointEdge->Measurement(mapUv[consitute(i, j)]);  // 像素
+                posepointEdge->setInformation(Mat22::Identity());          // 信息矩阵
+                auto rk = new g2o::RobustKernelHuber();          // 鲁棒核
+                rk->setDelta(chi2_th);
+                posepointEdge->setRobustKernel(rk);
+                optimizer.addEdge(posepointEdge);
+            }
+        }
+    optimizer.initializeOptimization();
+    optimizer.optimize(10);
     }
 
 };
